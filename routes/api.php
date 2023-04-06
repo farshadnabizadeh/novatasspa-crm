ea0eff5063901d2d8e872c00966c0b4784206950
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-04-06 11:21:54 +0300

    corsError

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ContactFormApiController;
 use App\Http\Controllers\Api\BookingFormApiController;
+# Headers To resolve Cors Error
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
 /*
 |--------------------------------------------------------------------------
 | API Routes
